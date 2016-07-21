8a18f99a210ac66f4a7e5ba2af852682294716cc
Michael Dickens <michael.dickens@affirm.com>
2016-07-20 18:04:01 -0700

    Initial commit

---
+++ writing-progress-tracker.el
@@ -0,0 +1,18 @@
+;; 
+;; writing-progress-tracker.el
+;; ---------------------------
+
+;; Author: Michael Dickens <mdickens93@gmail.com>
+;; Created: 2016-07-20
+
+;;; Commentary:
+
+;; description
+
+
+;;; Code:
+
+
+
+
+;;; writing-progress-tracker.el ends here
